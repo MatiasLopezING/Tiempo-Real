1351dd0fd5c71f8979fa0194b64ca99889dc5b7a
Matias Lopez <ml3280199@gmail.com>
2026-09-23 17:59:53 -0300

    trabajo hecho en clase, entrega 1 terminada

---
+++ TP1-Interrupciones/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 23/9/2026 14:33:54
+ * Author : ml328
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
--- TP1-Interrupciones/src/Parte1PollingyDelay.c
+++ TP1-Interrupciones/src/Parte1PollingyDelay.c
@@ -1,55 +1,42 @@
 /*
  * tp1SistemasTR.c
- *
- * Created: 6/9/2026 16:08:58
- * Author : Juan
- */ 
+ * Ejercicio 1: Control E/S digital por polling y retardos
+ */
 
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
-typedef enum {ENCENDIDO, APAGADO} estado_t;
+#define MASK_BOTONES ((1 << PIND2) | (1 << PIND3))
 
 void iniciar(void) {
-	DDRD &= ~((1 << PORTD2) | (1 << PORTD3)); //Entradas (0)
-	DDRB |= (1 << PORTB0) | (1 << PORTB1); //Salidas (1)
-	PORTD |= (1 << PORTD2) | (1 << PORTD3); //Resistencias pull-up
+	DDRD &= ~((1 << PORTD2) | (1 << PORTD3)); // PD2, PD3 como entradas
+	PORTD |= (1 << PORTD2) | (1 << PORTD3);   // Pull-up internas activadas
+	DDRB |= (1 << PORTB0) | (1 << PORTB1);    // PB0, PB1 como salidas
 }
 
 int main(void)
 {
-    iniciar();
-	estado_t estado = APAGADO;
-	uint8_t anterior = 0; //Encendido(1) : Apagado(0)
-	uint8_t toggle = 0; //Para el parpadeo
-    while (1) 
-    {
-		uint8_t bt1 = !(PIND & (1 << PIND2)); //Leemos un 0 (presiono btn) 
-		uint8_t bt2 = !(PIND & (1 << PIND3));
-		if (bt1 || bt2) {
-			if (anterior == 0) {
-			    estado = (estado == ENCENDIDO) ? APAGADO : ENCENDIDO;
-				anterior = 1;
-			} 
-		} else {
-			//No se registro btn presionado, reseteo		
-			anterior = 0;
-		}
-		if (estado == ENCENDIDO) {
-			if (toggle) {
-				PORTB |= (1 << PORTB0);   // Encendemos LED en PB0
-				PORTB &= ~(1 << PORTB1);   // Apagamos LED en PB1
-				toggle = 0; 
-			} else {
-				PORTB &= ~(1 << PORTB0);   // Apagamos LED en PB0
-				PORTB |= (1 << PORTB1);   // Encendemos LED en PB1
-				toggle = 1;
-			}
-		} else {
-			PORTB &= ~((1 << PORTB0) | (1 << PORTB1)); //Apagamos ambos leds
-		}
+	iniciar();
+
+	// a) Al iniciar, ambos LEDs encendidos
+	PORTB |= (1 << PORTB0) | (1 << PORTB1);
+
+	// b) Polling de PIND: se espera hasta que se presione cualquier pulsador
+	//    (con pull-up, presionado = 0)
+	while ((PIND & MASK_BOTONES) == MASK_BOTONES) {
+		// LEDs fijos encendidos, esperando pulsador
+	}
+
+	// b) y c) Parpadeo alternado, 250 ms por estado
+	while (1)
+	{
+		PORTB |=  (1 << PORTB0);   // PB0 encendido
+		PORTB &= ~(1 << PORTB1);   // PB1 apagado
+		_delay_ms(250);
+
+		PORTB &= ~(1 << PORTB0);   // PB0 apagado
+		PORTB |=  (1 << PORTB1);   // PB1 encendido
 		_delay_ms(250);
 	}
 }
-
